875f536180fa5981abd5ecd83c976b09fbda3a44
RuLe0G <khlebnikov2015@yandex.ru>
2019-02-22 14:06:34 +0700

    first

---
+++ l2/l2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace l2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            ConsoleKeyInfo Key = Console.ReadKey();
+            char ch = Key.KeyChar;
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
